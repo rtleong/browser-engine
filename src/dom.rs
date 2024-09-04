6f8c66db7906854fe99c49304a2d02b326eecf83
rtleong <rileyleong24@gmail.com>
2024-09-04 11:59:33 -0700

    feat: Add node and element data type structure

---
+++ src/dom.rs
@@ -0,0 +1,24 @@
+use std::collections::{HashMap, HashSet};
+
+pub type AttrMap = HashMap<String, String>;
+
+#[derive(Debug)]
+pub struct Node {
+    // data common to all nodes:
+    pub children: Vec<Node>,
+
+    // data specific to each node type:
+    pub node_type: NodeType,
+}
+
+#[derive(Debug)]
+pub enum NodeType {
+    Element(ElementData),
+    Text(String),
+}
+
+#[derive(Debug)]
+pub struct ElementData {
+    pub tag_name: String,
+    pub attrs: AttrMap,
+}
