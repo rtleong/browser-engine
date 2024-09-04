8b5182e28ab1786f650ec422b3822fee204390e2
rtleong <rileyleong24@gmail.com>
2024-09-04 12:42:04 -0700

    feat: Update layout of boxes

---
+++ src/layout.rs
@@ -0,0 +1,33 @@
+use crate::style::{StyledNode, Display};
+use crate::css::{Value::{Keyword, Length}, Unit::Px};
+use std::default::Default;
+
+pub use self::BoxType::{AnonymousBlock, InlineNode, BlockNode};
+
+// CSS box model. All sizes are in px.
+
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
+
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Dimensions {
+    /// Position of the content area relative to the document origin:
+    pub content: Rect,
+    // Surrounding edges:
+    pub padding: EdgeSizes,
+    pub border: EdgeSizes,
+    pub margin: EdgeSizes,
+}
+
+#[derive(Clone, Copy, Default, Debug)]
+pub struct EdgeSizes {
+    pub left: f32,
+    pub right: f32,
+    pub top: f32,
+    pub bottom: f32,
+}
