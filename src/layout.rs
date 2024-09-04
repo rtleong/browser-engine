68ed79162463e4803cc1306662f3db2e1744f731
rtleong <rileyleong24@gmail.com>
2024-09-04 12:45:12 -0700

    feat:Add layout box styling

--- src/layout.rs
+++ src/layout.rs
@@ -31,3 +31,34 @@ pub struct EdgeSizes {
     pub top: f32,
     pub bottom: f32,
 }
+
+
+/// A node in the layout tree.
+pub struct LayoutBox<'a> {
+    pub dimensions: Dimensions,
+    pub box_type: BoxType<'a>,
+    pub children: Vec<LayoutBox<'a>>,
+}
+
+pub enum BoxType<'a> {
+    BlockNode(&'a StyledNode<'a>),
+    InlineNode(&'a StyledNode<'a>),
+    AnonymousBlock,
+}
+
+impl<'a> LayoutBox<'a> {
+    fn new(box_type: BoxType) -> LayoutBox {
+        LayoutBox {
+            box_type,
+            dimensions: Default::default(), // initially set all fields to 0.0
+            children: Vec::new(),
+        }
+    }
+
+    fn get_style_node(&self) -> &'a StyledNode<'a> {
+        match self.box_type {
+            BlockNode(node) | InlineNode(node) => node,
+            AnonymousBlock => panic!("Anonymous block box has no style node")
+        }
+    }
+}
