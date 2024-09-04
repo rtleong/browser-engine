8c088da54ebeaac3b78434e3871d8856666e1846
rtleong <rileyleong24@gmail.com>
2024-09-04 12:35:36 -0700

    feat: Full implementation of Parser

--- src/css.rs
+++ src/css.rs
@@ -72,3 +72,191 @@ impl Value {
         }
     }
 }
+
+/// Parse a whole CSS stylesheet.
+pub fn parse(source: String) -> Stylesheet {
+    let mut parser = Parser { pos: 0, input: source };
+    Stylesheet { rules: parser.parse_rules() }
+}
+
+struct Parser {
+    pos: usize,
+    input: String,
+}
+
+impl Parser {
+    /// Parse a list of rule sets, separated by optional whitespace.
+    fn parse_rules(&mut self) -> Vec<Rule> {
+        let mut rules = Vec::new();
+        loop {
+            self.consume_whitespace();
+            if self.eof() { break }
+            rules.push(self.parse_rule());
+        }
+        rules
+    }
+
+    /// Parse a rule set: `<selectors> { <declarations> }`.
+    fn parse_rule(&mut self) -> Rule {
+        Rule {
+            selectors: self.parse_selectors(),
+            declarations: self.parse_declarations(),
+        }
+    }
+
+    /// Parse a comma-separated list of selectors.
+    fn parse_selectors(&mut self) -> Vec<Selector> {
+        let mut selectors = Vec::new();
+        loop {
+            selectors.push(Selector::Simple(self.parse_simple_selector()));
+            self.consume_whitespace();
+            match self.next_char() {
+                ',' => { self.consume_char(); self.consume_whitespace(); }
+                '{' => break,
+                c   => panic!("Unexpected character {} in selector list", c)
+            }
+        }
+        // Return selectors with highest specificity first, for use in matching.
+        selectors.sort_by_key(|s| s.specificity());
+        selectors
+    }
+
+    /// Parse one simple selector, e.g.: `type#id.class1.class2.class3`
+    fn parse_simple_selector(&mut self) -> SimpleSelector {
+        let mut selector = SimpleSelector { tag_name: None, id: None, class: Vec::new() };
+        while !self.eof() {
+            match self.next_char() {
+                '#' => {
+                    self.consume_char();
+                    selector.id = Some(self.parse_identifier());
+                }
+                '.' => {
+                    self.consume_char();
+                    selector.class.push(self.parse_identifier());
+                }
+                '*' => {
+                    // universal selector
+                    self.consume_char();
+                }
+                c if valid_identifier_char(c) => {
+                    selector.tag_name = Some(self.parse_identifier());
+                }
+                _ => break
+            }
+        }
+        selector
+    }
+
+    /// Parse a list of declarations enclosed in `{ ... }`.
+    fn parse_declarations(&mut self) -> Vec<Declaration> {
+        self.expect_char('{');
+        let mut declarations = Vec::new();
+        loop {
+            self.consume_whitespace();
+            if self.next_char() == '}' {
+                self.consume_char();
+                break;
+            }
+            declarations.push(self.parse_declaration());
+        }
+        declarations
+    }
+
+    /// Parse one `<property>: <value>;` declaration.
+    fn parse_declaration(&mut self) -> Declaration {
+        let name = self.parse_identifier();
+        self.consume_whitespace();
+        self.expect_char(':');
+        self.consume_whitespace();
+        let value = self.parse_value();
+        self.consume_whitespace();
+        self.expect_char(';');
+
+        Declaration { name, value }
+    }
+
+    // Methods for parsing values:
+
+    fn parse_value(&mut self) -> Value {
+        match self.next_char() {
+            '0'..='9' => self.parse_length(),
+            '#' => self.parse_color(),
+            _ => Value::Keyword(self.parse_identifier())
+        }
+    }
+
+    fn parse_length(&mut self) -> Value {
+        Value::Length(self.parse_float(), self.parse_unit())
+    }
+
+    fn parse_float(&mut self) -> f32 {
+        self.consume_while(|c| matches!(c, '0'..='9' | '.')).parse().unwrap()
+    }
+
+    fn parse_unit(&mut self) -> Unit {
+        match &*self.parse_identifier().to_ascii_lowercase() {
+            "px" => Unit::Px,
+            _ => panic!("unrecognized unit")
+        }
+    }
+
+    fn parse_color(&mut self) -> Value {
+        self.expect_char('#');
+        Value::ColorValue(Color {
+            r: self.parse_hex_pair(),
+            g: self.parse_hex_pair(),
+            b: self.parse_hex_pair(),
+            a: 255 })
+    }
+
+    /// Parse two hexadecimal digits.
+    fn parse_hex_pair(&mut self) -> u8 {
+        let s = &self.input[self.pos .. self.pos + 2];
+        self.pos += 2;
+        u8::from_str_radix(s, 16).unwrap()
+    }
+
+    /// Parse a property name or keyword.
+    fn parse_identifier(&mut self) -> String {
+        self.consume_while(valid_identifier_char)
+    }
+
+    /// Consume and discard zero or more whitespace characters.
+    fn consume_whitespace(&mut self) {
+        self.consume_while(char::is_whitespace);
+    }
+
+    /// Consume characters until `test` returns false.
+    fn consume_while(&mut self, test: impl Fn(char) -> bool) -> String {
+        let mut result = String::new();
+        while !self.eof() && test(self.next_char()) {
+            result.push(self.consume_char());
+        }
+        result
+    }
+
+    /// Return the current character, and advance self.pos to the next character.
+    fn consume_char(&mut self) -> char {
+        let c = self.next_char();
+        self.pos += c.len_utf8();
+        c
+    }
+
+    /// If the exact string `s` is found at the current position, consume it.
+    /// Otherwise, panic.
+    fn expect_char(&mut self, c: char) {
+        if self.consume_char() != c {
+            panic!("Expected {:?} at byte {} but it was not found", c, self.pos);
+        }
+    }
+
+    /// Read the current character without consuming it.
+    fn next_char(&self) -> char {
+        self.input[self.pos..].chars().next().unwrap()
+    }
+
+    /// Return true if all input is consumed.
+    fn eof(&self) -> bool {
+        self.pos >= self.input.len()
+    }
+}
