14a5098560ce5f72f0651b668f464bf0ef8b1b42
rtleong <rileyleong24@gmail.com>
2024-09-04 12:26:16 -0700

    feat: Add multiple new supported structures for parsing

--- src/html.rs
+++ src/html.rs
@@ -41,3 +41,41 @@ impl Parser {
             self.parse_text()
         }
     }
+
+      /// Parse a single element, including its open tag, contents, and closing tag.
+      fn parse_element(&mut self) -> dom::Node {
+        // Opening tag.
+        self.expect("<");
+        let tag_name = self.parse_name();
+        let attrs = self.parse_attributes();
+        self.expect(">");
+
+        // Contents.
+        let children = self.parse_nodes();
+
+        // Closing tag.
+        self.expect("</");
+        self.expect(&tag_name);
+        self.expect(">");
+
+        dom::elem(tag_name, attrs, children)
+    }
+
+    /// Parse a tag or attribute name.
+    fn parse_name(&mut self) -> String {
+        self.consume_while(|c| matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9'))
+    }
+
+    /// Parse a list of name="value" pairs, separated by whitespace.
+    fn parse_attributes(&mut self) -> dom::AttrMap {
+        let mut attributes = HashMap::new();
+        loop {
+            self.consume_whitespace();
+            if self.next_char() == '>' {
+                break;
+            }
+            let (name, value) = self.parse_attr();
+            attributes.insert(name, value);
+        }
+        attributes
+    }
