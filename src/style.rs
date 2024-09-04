d5f429cc7efcc576740e728e839cb2f1a5942210
rtleong <rileyleong24@gmail.com>
2024-09-04 12:37:59 -0700

    feat: Implementation of StyledNode

--- src/style.rs
+++ src/style.rs
@@ -11,3 +11,36 @@ pub struct StyledNode<'a> {
     pub specified_values: PropertyMap,
     pub children: Vec<StyledNode<'a>>,
 }
+
+#[derive(PartialEq)]
+pub enum Display {
+    Inline,
+    Block,
+    None,
+}
+
+impl<'a> StyledNode<'a> {
+    /// Return the specified value of a property if it exists, otherwise `None`.
+    pub fn value(&self, name: &str) -> Option<Value> {
+        self.specified_values.get(name).cloned()
+    }
+
+    /// Return the specified value of property `name`, or property `fallback_name` if that doesn't
+    /// exist, or value `default` if neither does.
+    pub fn lookup(&self, name: &str, fallback_name: &str, default: &Value) -> Value {
+        self.value(name).unwrap_or_else(|| self.value(fallback_name)
+                        .unwrap_or_else(|| default.clone()))
+    }
+
+    /// The value of the `display` property (defaults to inline).
+    pub fn display(&self) -> Display {
+        match self.value("display") {
+            Some(Value::Keyword(s)) => match &*s {
+                "block" => Display::Block,
+                "none" => Display::None,
+                _ => Display::Inline
+            },
+            _ => Display::Inline
+        }
+    }
+}
