fe995bdbc5ac25fcb3c028ac9731263b179fc348
rtleong <rileyleong24@gmail.com>
2024-09-04 12:51:00 -0700

    feat: Render edges and borders

--- src/painting.rs
+++ src/painting.rs
@@ -37,3 +37,51 @@ fn render_layout_box(list: &mut DisplayList, layout_box: &LayoutBox) {
         render_layout_box(list, child);
     }
 }
+
+fn render_background(list: &mut DisplayList, layout_box: &LayoutBox) {
+    if let Some(color) = get_color(layout_box, "background") {
+        list.push(DisplayCommand::SolidColor(color, layout_box.dimensions.border_box()));
+    }
+}
+
+fn render_borders(list: &mut DisplayList, layout_box: &LayoutBox) {
+    let color = match get_color(layout_box, "border-color") {
+        Some(color) => color,
+        _ => return
+    };
+
+    let d = &layout_box.dimensions;
+    let border_box = d.border_box();
+
+    // Left border
+    list.push(DisplayCommand::SolidColor(color, Rect {
+        x: border_box.x,
+        y: border_box.y,
+        width: d.border.left,
+        height: border_box.height,
+    }));
+
+    // Right border
+    list.push(DisplayCommand::SolidColor(color, Rect {
+        x: border_box.x + border_box.width - d.border.right,
+        y: border_box.y,
+        width: d.border.right,
+        height: border_box.height,
+    }));
+
+    // Top border
+    list.push(DisplayCommand::SolidColor(color, Rect {
+        x: border_box.x,
+        y: border_box.y,
+        width: border_box.width,
+        height: d.border.top,
+    }));
+
+    // Bottom border
+    list.push(DisplayCommand::SolidColor(color, Rect {
+        x: border_box.x,
+        y: border_box.y + border_box.height - d.border.bottom,
+        width: border_box.width,
+        height: d.border.bottom,
+    }));
+}
